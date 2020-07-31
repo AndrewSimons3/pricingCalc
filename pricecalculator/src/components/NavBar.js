fe5f3deb44c81e83a07d57134b648e25a062cbf6
AndrewSimons3 <simdrew07@gmail.com>
2020-07-30 21:19:07 -0500

    updated nav bar

--- pricecalculator/src/components/NavBar.js
+++ pricecalculator/src/components/NavBar.js
@@ -3,42 +3,74 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/MenuIcon';
 import { Link } from 'react-router-dom';
 import { logOut } from '../checkAuth';
-
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
- 
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   title: {
     flexGrow: 1,
-    display: "flex"
   },
 }));
 
-export default function NavBar(props) {
+export default function ButtonAppBar(props) {
   const isAuthenticated = props.loggedIn
   const loginButtonText = isAuthenticated ? "Logout" : "Login"
   const classes = useStyles();
 
-  console.log(isAuthenticated)
-
   return (
     <div className={classes.root}>
-      <AppBar position="static" style= {{background: '#4caf50'}}>
+      <AppBar position="static">
         <Toolbar>
-          <Typography edge="start" variant="h6" className={classes.title}>
-            Austin Small Business
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
           </Typography>
-          <Link className='link' to='/'>Listings</Link>
-          {isAuthenticated && (<Link className='link' to='/add'>Add</Link>)}
+          <Button color="inherit">Login</Button>
           <Link className='link' onClick={logOut} to='/login'>{loginButtonText}</Link>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import { Link } from 'react-router-dom';
+// import { logOut } from '../checkAuth';
+
+// export default function NavBar(props) {
+//   const isAuthenticated = props.loggedIn
+//   const loginButtonText = isAuthenticated ? "Logout" : "Login"
+//   const classes = useStyles();
+
+//   console.log(isAuthenticated)
+
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static" style= {{background: '#4caf50'}}>
+//         <Toolbar>
+//           <Typography edge="start" variant="h6" className={classes.title}>
+//             Austin Small Business
+//           </Typography>
+//           <Link className='link' to='/'>Listings</Link>
+//           {isAuthenticated && (<Link className='link' to='/add'>Add</Link>)}
+          // <Link className='link' onClick={logOut} to='/login'>{loginButtonText}</Link>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
