12cd9af545f640b8d521b4eb4a4b625f06b041b7
AndrewSimons3 <simdrew07@gmail.com>
2020-07-31 07:51:03 -0500

    updated login page img

--- pricecalculator/src/components/Login.js
+++ pricecalculator/src/components/Login.js
@@ -1,40 +1,4 @@
-// import React from 'react'
-// import TextField from '@material-ui/core/TextField'
-// import Button from '@material-ui/core/Button'
-
-
-
-// const Login = () => {
-//   const history = useHistory()
-
-//   const userLoggedIn = (e) => {
-//     e.preventDefault()
-//     document.cookie = "loggedIn=true;max-age=60*1000"
-//     history.push("/")
-
-//   };
-//   return (
-//     <div>
-//       <NavBar loggedIn={false}></NavBar>
-//          <div className="App">
-//       <form>
-//         <br />
-//         <TextField id="standard-basic" label="Username*"style={{width: "40%"}}/>
-//         <br />
-//         <TextField id="standard-basic" label="Password*" style={{width: "40%"}} />
-//         <br />
-//         <Button onClick={userLoggedIn} type="submit" variant="contained" color="inherit" style={{margin: "30px", width:"40%"}} >
-//           Login
-//         </Button>
-//       </form>
-//     </div> 
-//     </div>
-//   )
-// }
-
-  
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -48,7 +12,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useHistory } from 'react-router-dom'
-import NavBar from './NavBar'
 
 function Copyright() {
 
@@ -98,12 +61,7 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          {/* <LockOutlinedIcon /> */}
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
+        {<img src="https://360homeconnect.com/wp-content/uploads/2018/01/logo.png" alt="360 Home Connect" width="300" height="150"></img>}
         <form className={classes.form} noValidate>
           <TextField
             variant="outlined"
@@ -159,4 +117,3 @@ export default function SignIn() {
     </Container>
   );
 }
-// export default Login;
