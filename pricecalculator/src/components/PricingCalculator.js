855bdb82e8c35fa9fd258c2c8eb2cf1bd2218ebf
AndrewSimons3 <simdrew07@gmail.com>
2020-08-01 18:11:26 -0500

    added equipment selector but not mapping

--- pricecalculator/src/components/PricingCalculator.js
+++ pricecalculator/src/components/PricingCalculator.js
@@ -33,11 +33,12 @@ class PricingCalculator extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {selectedProductId: "", selectedPackageId: ""}
+    this.state = {selectedProductId: "", selectedPackageId: "", selectedEquipmentId: ""}
 
 
     this.updateProductSelect = this.updateProductSelect.bind(this)
     this.updatePackageSelect = this.updatePackageSelect.bind(this)
+    this.updateEquipmentSelect = this.updateEquipmentSelect.bind(this)
 
   }
 
@@ -51,21 +52,32 @@ class PricingCalculator extends React.Component {
     const productId = event.target.value
     const product = this.getProduct(productId)
     const packages = product.packages
+    const equipment = product.equipment
     var packageId = undefined
+    var equipmentId = undefined
     if(packages) {
       packageId = packages[0].name
     }
     this.setState({selectedProductId: event.target.value, selectedPackageId: packageId})
     console.log(event.target.value)
+
+    if(equipment) {
+      equipmentId = equipment[0].name
+    }
+    this.setState({selectedProductId: event.target.value, selectedEquipmentId: equipmentId})
   }
 
   updatePackageSelect(event) {
     this.setState({selectedPackageId: event.target.value})
   }
 
+  updateEquipmentSelect(event) {
+    this.setState({selectedEquipmentId: event.target.value})
+  }
+
   getProduct(productId) {
     return this.props.products.find(product => {
-      return product.name == productId
+      return product.name === productId
     })
   }
 
@@ -77,32 +89,41 @@ class PricingCalculator extends React.Component {
     return product.packages
   }
 
+  getEquipment(products) {
+    if (!this.state.selectedProductId) {
+      return null
+    }
+    const product = this.getProduct(this.state.selectedProductId)
+    return product.equipment
+  }
+
   render() {
 
     const { products, classes } = this.props;
     const packages = this.getPackages(products)
+    const equipment = this.getEquipment(products)
     console.log(packages);
 
     if (products && products.length > 0) {
       return (
         <div className={classes.formWrapper}>
-        <FormControl className={classes.formControl}>
-          <div className={classes.fullWidth}>
-           <InputLabel id="product-select">Products</InputLabel>
-            <Select className={classes.fullWidth}
-              labelId="products"
-              id="product-select"
-              value={this.state.selectedProductId}
-              onChange={this.updateProductSelect}>
-              {
-                products.map((product, key) => {
-                  console.log(product.id)
-                  return <MenuItem value={product.name} key={key}>{product.name}</MenuItem>
-              })
-              }
-            </Select>
-          </div>
-        </FormControl>
+          <FormControl className={classes.formControl}>
+            <div className={classes.fullWidth}>
+            <InputLabel id="product-select">Products</InputLabel>
+              <Select className={classes.fullWidth}
+                labelId="products"
+                id="product-select"
+                value={this.state.selectedProductId}
+                onChange={this.updateProductSelect}>
+                {
+                  products.map((product, key) => {
+                    console.log(product.id)
+                    return <MenuItem value={product.name} key={key}>{product.name}</MenuItem>
+                  })
+                }
+              </Select>
+            </div>
+          </FormControl>
             {this.state.selectedProductId && (
               <FormControl className={classes.formControl}>
                 <div className={classes.fullWidth}>
@@ -121,6 +142,22 @@ class PricingCalculator extends React.Component {
                 </div>
               </FormControl>
             )}
+              <FormControl className={classes.formControl}>
+                <div className={classes.fullWidth}>
+                 <InputLabel id="equipment-select">Equipment</InputLabel>
+                 <Select className={classes.fullWidth}
+                    labelId="equipment"
+                    id="equipment-select"
+                    value={this.state.selectedEquipmentId}
+                    onChange={this.updateEquipmentSelect}>
+                    {
+                      equipment.map((equipment, key) => {
+                        return <MenuItem value={equipment.name} key={key}>{equipment.name}</MenuItem>
+                      })
+                    }
+                  </Select>
+              </div>
+            </FormControl>
       </div>
       );
     }
