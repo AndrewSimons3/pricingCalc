a162a643352f4b3be43d6b228b2a5289b79b0b45
AndrewSimons3 <simdrew07@gmail.com>
2020-08-11 20:01:23 -0500

    updated an electric rate

--- pricecalculator/src/components/Electric.js
+++ pricecalculator/src/components/Electric.js
@@ -196,7 +196,7 @@ export default function AutoGrid() {
           <Paper className={classes.paper}>10.5 2000kwph<br></br>10.8 1000kwph<br></br>11.3 500kwph</Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>10.5 2000kwph<br></br>10.8 1000kwph<br></br>11.3 500kwph</Paper>
+          <Paper className={classes.paper}>12 2000kwph<br></br>10.8 1000kwph<br></br>11.3 500kwph</Paper>
         </Grid>
       </Grid>
       <Grid container>
