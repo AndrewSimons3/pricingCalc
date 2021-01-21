2a3b7290dbf84abc9a0c21c7c11718c4e288a66f
AndrewSimons3 <simdrew07@gmail.com>
2021-01-20 22:22:18 -0600

    test

--- pricecalculator/src/components/PricingCalculator.js
+++ pricecalculator/src/components/PricingCalculator.js
@@ -440,7 +440,7 @@ class PricingCalculator extends React.Component {
                     {this.getCurrency(speedWithPrice.install)}
                     </Typography>
                     <Typography className={classes.title} color="textSecondary" gutterBottom>
-                      Total Monthly Cost
+                      Total Monthly Cost!
                     </Typography>
                     <Typography className={classes.price} variant="h5" component="h2">
                       {this.getCurrency(speedWithPrice.intPrice)}
